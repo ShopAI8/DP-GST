914890c8b65c2e09edc4eb351e9d5efb0d1e2f4d
Jiakang Li <148515902+ShopAI8@users.noreply.github.com>
2025-07-04 17:52:41 +0800

    Update train-gcn.sh

--- DP-GST/train-gcn.sh
+++ DP-GST/train-gcn.sh
@@ -1,6 +1,5 @@
 export PYTHONPATH="$PWD:$PYTHONPATH"
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-#   --training_split "/your/tsp500_train_concorde.txt" \/your/storage/path
 python -u train.py \
   --task "gst" \
   --model "gcn" \
@@ -12,10 +11,10 @@ python -u train.py \
   --loss_type_GCN "FL" \
   --aggregation_GCN 'mean' \
   --learning_rate_GCN 1e-5 \
-  --storage_path "./" \
-  --training_split "../data/tasks_30-num_2000-epsilon_1-delta_0.05/dblp_GST1k-gamma_3-noise/train_split.txt" \
-  --validation_split "../data/tasks_30-num_2000-epsilon_1-delta_0.05/dblp_GST1k-gamma_3-noise/valid_split.txt" \
-  --test_split "../data/tasks_30-num_2000-epsilon_1-delta_0.05/dblp_GST1k-gamma_3-noise/test_split.txt" \
+  --storage_path "/your/storage/path" \
+  --training_split "/your/tasks_30-num_2000-epsilon_1-delta_0.05/dblp_GST1k-gamma_3-noise/train_split.txt" \
+  --validation_split "/your/tasks_30-num_2000-epsilon_1-delta_0.05/dblp_GST1k-gamma_3-noise/valid_split.txt" \
+  --test_split "/your/tasks_30-num_2000-epsilon_1-delta_0.05/dblp_GST1k-gamma_3-noise/test_split.txt" \
   --batch_size 3 \
   --num_epochs 15 \
   --validation_examples 64 \
