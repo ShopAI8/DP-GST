f25a2009916bbb4e41c50a4bf6bdd3dba9b98dd5
Jiakang Li <148515902+ShopAI8@users.noreply.github.com>
2025-07-04 19:46:36 +0800

    Create gst_data_noise.sh

---
+++ data/gst_data_noise.sh
@@ -0,0 +1,9 @@
+python -u gst_data_noise.py \
+  --gam 3 \
+  --epsilon 1 \
+  --num_train 1600 \
+  --num_test_val 40 \
+  --num_query_task 30 \
+  --delta 0.05 \
+  --dataname "dblp" \
+  --seed 1234
